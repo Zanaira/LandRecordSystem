682b8dea79273033d05673e40bd2dd0e6e1cba95
Zanaira <zanaira078@gmail.com>
2026-04-18 02:49:27 +0500

    Update API base URL to use environment variable

--- LandRecord_frontend/src/services/api.js
+++ LandRecord_frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { getToken } from '../utils/auth';
 
 const API = axios.create({
-  baseURL: "https://landrecordsystem-backend.onrender.com", // your backend URL
+  baseURL: `${REACT_APP_API_URL}`, // your backend URL
 });
 
 // Attach token automatically
